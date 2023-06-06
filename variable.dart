58e12211a6321f88a18c0a1231578be8c77f4831
LEEBONGHAK <dlqhdgkr456@gmail.com>
2023-06-06 14:01:54 +0900

    2 variable

---
+++ variable.dart
@@ -0,0 +1,14 @@
+void main() {
+  // 변수 선언
+  var name = 'Hello World!!';
+  print(name);
+
+  var name2 = 'Lee';
+  print(name2);
+
+  name = '플러터 베이직';
+  print(name);
+
+  // error
+  // var name = 'hello world2';
+}
