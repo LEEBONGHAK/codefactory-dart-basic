84c88f3575590535001934d8efb45dd1bab6f92d
LEEBONGHAK <dlqhdgkr456@gmail.com>
2023-06-06 17:15:54 +0900

    27 spread operator

---
+++ functional-programming/spread-operator.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<int> even = [2, 4, 6, 8];
+  List<int> odd = [1, 3, 5, 7];
+
+  // spread operator
+  // ...
+  print([...even, ...odd]);
+  print(even);
+  print([...even]);
+  print(even == [...even]);
+}
